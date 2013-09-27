f9b53f248618a8d8ebd8ce9e3c0f06327cf3a8ec
Marc Grimme <grimme@atix.de>
2013-09-27 09:53:12 +0200

    [vSphere] Support datacenters that are located below folders not in root folder

--- lib/fog/vsphere/requests/compute/list_datacenters.rb
+++ lib/fog/vsphere/requests/compute/list_datacenters.rb
@@ -8,12 +8,21 @@ module Fog
             {
               :id => managed_obj_id(dc),
               :name => dc.name,
+              :path => raw_getpathmo(dc),
               :status => dc.overallStatus
             }
           end
         end
 
         protected
+
+        def raw_getpathmo mo 
+          if mo.parent == nil or mo.parent.name == @connection.rootFolder.name then
+            [ mo.name ]
+          else
+            [ *raw_getpathmo(mo.parent), mo.name ] 
+          end
+        end
 
         def raw_datacenters
           @raw_datacenters ||= @connection.rootFolder.childEntity.grep(RbVmomi::VIM::Datacenter)
